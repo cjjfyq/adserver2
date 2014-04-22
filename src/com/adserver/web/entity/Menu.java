390726fe76b79c0842b79dd60200c46205dfede2
fyqjnu <fyqjnu@qq.com>
2014-04-22 19:38:20 +0800

    菜单实体类

---
+++ src/com/adserver/web/entity/Menu.java
@@ -0,0 +1,34 @@
+package com.adserver.web.entity;
+
+/**
+ * @author Administrator
+ * 菜单
+ */
+public class Menu {
+    /**
+     * 主键
+     */
+    private int id;
+    
+    private int menuId;
+    
+    /**
+     * 父菜单id
+     */
+    private int parentId;
+    
+    private String name;
+    
+    private String url;
+    
+    private int orderNum;
+    
+    /**
+     * 备注
+     */
+    private String command;
+    
+    
+    
+    
+}
