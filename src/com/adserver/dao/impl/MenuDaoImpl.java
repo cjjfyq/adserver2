089e4eea19fc6ad9c374435d0d9eedba6851fb74
fyqjnu <fyqjnu@qq.com>
2014-04-24 00:07:48 +0800

    添加菜单实体类Menu的Dao类

---
+++ src/com/adserver/dao/impl/MenuDaoImpl.java
@@ -0,0 +1,13 @@
+package com.adserver.dao.impl;
+
+import com.adserver.dao.AbstractBaseDao;
+import com.adserver.web.entity.Menu;
+
+/**
+ * @author Gordon
+ * 菜单实体类
+ */
+public class MenuDaoImpl extends AbstractBaseDao<Menu, Integer> {
+    
+
+}
